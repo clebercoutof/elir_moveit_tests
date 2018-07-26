169aa8d910e87f676e25c0cac37ca16fa9ea65d4
Cleber Couto Filho <clebercoutof@gmail.com>
2018-07-26 14:00:00 -0300

    add elir_description_openrave package and add base_link visual

--- urdf_to_collada.sh
+++ urdf_to_collada.sh
@@ -3,8 +3,7 @@
 #It is necessary to go robot urdf folder
 
 export MYROBOT_NAME="elir"
-export URDF_LOCATION="/home/mega/catkin_ws/src/elir_tests/elir_description_with_ref/urdf"
-export DAE_LOCATION="/home/mega/catkin_ws/src/elir_tests/elir_description_with_ref/urdf"
-roscore
+export URDF_LOCATION="/home/mega/catkin_ws/src/elir_tests/elir_description_openrave/urdf"
+export DAE_LOCATION="/home/mega/catkin_ws/src/elir_tests/elir_description_openrave/urdf"
 rosrun xacro xacro --inorder -o "$URDF_LOCATION"/"$MYROBOT_NAME".urdf "$MYROBOT_NAME".urdf.xacro
 rosrun collada_urdf urdf_to_collada "$URDF_LOCATION"/"$MYROBOT_NAME".urdf "$DAE_LOCATION"/"$MYROBOT_NAME".dae
